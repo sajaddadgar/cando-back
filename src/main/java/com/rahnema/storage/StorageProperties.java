adfdb05378044f05525c8f2652573fc8086bd4e6
Mahdi <erfanianmahdi00@gmail.com>
2019-08-06 14:25:35 +0430

    Change storage address to absolute mode

--- src/main/java/com/rahnema/storage/StorageProperties.java
+++ src/main/java/com/rahnema/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "upload-dir";
+    private String location = System.getProperty("user.dir") + "/upload-dir";
     private String avatarsLocation = location + "/avatars";
     private String bannersLocation = location + "/banners";
 
